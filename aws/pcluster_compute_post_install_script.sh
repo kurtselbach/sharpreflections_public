a6addc8abc94dde15effe175cdd317f3940da488
AndriiZaloga <87391862+AndriiZaloga@users.noreply.github.com>
2023-07-25 16:55:11 +0200

    Update pcluster_compute_post_install_script.sh

--- aws/pcluster_compute_post_install_script.sh
+++ aws/pcluster_compute_post_install_script.sh
@@ -1,1 +1,7 @@
+#!/bin/bash -e
 
+### PreStackPro need this packages ###
+sudo yum install -y jq libGLU libXtst xauth numactl libXcomposite libXcursor fontconfig libXrandr libEGL libX11 infiniband-diags libibverbs-utils qt5-qtbase-gui snapd-qt-qml qt5-qtsvg qt5-qtwebchannel qt5-qtx11extras qt5-qtwebengine mesa-libGLU qt5-qtlocation libxkbcommon-x11
+
+### memlock settings ###
+sudo echo "*          -       memlock   unlimited" >> /etc/security/limits.d/10-memlock.conf
