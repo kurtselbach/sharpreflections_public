4dd6ac65e2832d2a12d32c42e10f454755d3fca0
AndriiZaloga <87391862+AndriiZaloga@users.noreply.github.com>
2023-07-26 13:21:50 +0200

    Update start.sh

--- aws/start.sh
+++ aws/start.sh
@@ -19,8 +19,13 @@ if [[ "$(sinfo | grep -w -E 'idle~|alloc' | grep -v 'alloc#' | awk '{print $4}' 
 	exit
 fi
 
+cat << EOF >/data_parallel/job.sh
+#!/bin/bash
+tail -f /dev/null
+EOF
+
 if [[ "$1" ]]; then
-	sbatch -N $1 /shared/job.sh > /tmp/sbatch.log
+	sbatch -N $1 /data_parallel/job.sh > /tmp/sbatch.log
 fi
 
 job=$(awk '{print $4}' /tmp/sbatch.log)
@@ -42,15 +47,18 @@ done
 
 echo 'Start PreStackPro'
 
-NodeFile=/shared/NodeFile
+NodeFile=/data_parallel/NodeFile
 rm -rf $NodeFile && touch $NodeFile
 
-nodes=$(sinfo -o "%o" | grep -v -E 'hpc|NODE_ADDR' | xargs)
+#nodes=$(sinfo -o "%o" | grep -v -E 'hpc|NODE_ADDR' | xargs)
 
-for i in $nodes
-do
-	grep -w $i /etc/hosts | awk '{print $3}' >> $NodeFile
-done
+#for i in $nodes
+#do
+#	grep -w $i /etc/hosts | awk '{print $3}' >> $NodeFile
+#done
+
+nodes=$(sinfo --states=MIX -hN | awk '{print $1}')
+sinfo --states=MIX -hN | awk '{print $1}' > $NodeFile
 
 pspsetting_ramfactor=".70"
 usemem="99999999999999"
@@ -68,15 +76,26 @@ memtotal=$(echo $usemem| xargs -I {} echo "scale=1; {}/1024^2" | bc)
 memforuse=$(echo "$memtotal * $pspsetting_ramfactor" | bc -l)
 pspsetting_ram=$(echo $memforuse | awk '{printf("%d\n",$1 + 0.5)}')
 
-/shared/PreStackPro/662/bin/PreStackPro -a -u pspuser -b /shared/PreStackPro/662/bin/PreStackProBackend --ssh-private-key ~/.ssh/id_rsa -m $(head -1 $NodeFile) --nodefile $NodeFile -s $pspsetting_ram -p /shared \
---shutdown-script /shared/shutdown.sh
+# Add shutdown script
+
+cat > /data_parallel/shutdown.sh <<'EOF'
+#!/bin/bash
+for i in $(squeue | grep hpc | awk '{print $1}' | xargs)
+do
+	scancel $i
+done
+EOF
+chmod +x /data_parallel/shutdown.sh
+
+/data_parallel/PreStackPro/bin/PreStackPro -a -u $(echo $USER) -b /data_parallel/PreStackPro/bin/PreStackProBackend --ssh-private-key ~/.ssh/id_rsa -m $(head -1 $NodeFile) --nodefile $NodeFile -s $pspsetting_ram -p /data_parallel \
+--shutdown-script /data_parallel/shutdown.sh
 
 echo
 echo 'Check slurm jobs. Waiting...'
 sleep 3
 
 if [[ $(squeue | grep hpc ) ]]; then
-	echo 'You need to remove all jobs manually or start /shared/shutdown.sh'
+	echo 'You need to remove all jobs manually or start /data_parallel/shutdown.sh'
 else
 	echo 'Delete all jobs.'
 fi
