281079080af4c879894e63f7edf45fe4f3792d65
AndriiZaloga <87391862+AndriiZaloga@users.noreply.github.com>
2023-07-26 09:37:26 +0200

    Create pcluster_headnode_post_install_script.sh

---
+++ aws/pcluster_headnode_post_install_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+### PreStackPro need this packages ###
+sudo yum install -y jq screen libGLU libXtst xauth numactl libXcomposite libXcursor fontconfig \
+	libXrandr libEGL libX11 infiniband-diags libibverbs-utils qt5-qtbase-gui snapd-qt-qml qt5-qtsvg \
+	qt5-qtwebchannel qt5-qtx11extras qt5-qtwebengine mesa-libGLU qt5-qtlocation libxkbcommon-x11 pciutils bc
+
+### memlock settings ###
+sudo echo "*          -       memlock   unlimited" >> /etc/security/limits.d/10-memlock.conf
+
+# slurm
+sudo sed -i 's/ResumeTimeout=1800/ResumeTimeout=800/g' /etc/slurm/slurm.conf
+sudo systemctl restart slurmctld
+sudo systemctl enable slurmctld
