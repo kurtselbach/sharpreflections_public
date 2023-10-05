aec601a978f10e8872657c388186132eec1712ff
kurtselbach <32711195+kurtselbach@users.noreply.github.com>
2023-10-05 19:02:08 +0200

    Update deploy.sh

--- wsl/deploy.sh
+++ wsl/deploy.sh
@@ -3,5 +3,4 @@ clear
 echo "Starting Sharp Reflections WSL Deployment script"
 echo "Updating system and installing requried packages"
 apt-get update
-apt-get install
-apt-get -y install sshfs rpm2cpio libegl1 libglu1-mesa libibverbs1 libfreetype6 libnss3 libxtst6 libxcomposite1 qt5* libfontconfig mesa-utils keychain fping
+apt-get install sshfs rpm2cpio libegl1 libglu1-mesa libibverbs1 libfreetype6 libnss3 libxtst6 libxcomposite1 qt5* libfontconfig mesa-utils keychain fping -y
