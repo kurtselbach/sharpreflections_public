f7047f268ecac1087ab2da138ae3e8322d1a15cd
AndriiZaloga <87391862+AndriiZaloga@users.noreply.github.com>
2023-11-06 16:28:28 +0100

    Update pcluster_headnode_post_install_script.sh

--- aws/pcluster_headnode_post_install_script.sh
+++ aws/pcluster_headnode_post_install_script.sh
@@ -3,7 +3,7 @@
 ### PreStackPro need this packages ###
 sudo yum install -y jq screen libGLU libXtst xauth numactl libXcomposite libXcursor fontconfig \
 	libXrandr libEGL libX11 infiniband-diags libibverbs-utils qt5-qtbase-gui snapd-qt-qml qt5-qtsvg \
-	qt5-qtwebchannel qt5-qtx11extras qt5-qtwebengine mesa-libGLU qt5-qtlocation libxkbcommon-x11 pciutils bc wget
+	qt5-qtwebchannel qt5-qtx11extras mesa-libGLU qt5-qtlocation libxkbcommon-x11 pciutils bc wget #qt5-qtwebengine
 
 ### memlock settings ###
 sudo echo "*          -       memlock   unlimited" >> /etc/security/limits.d/10-memlock.conf
